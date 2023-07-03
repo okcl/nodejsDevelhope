e54c748c4607cb9ddc0bd3f7e94e1de15715d8bd
Oğuzhan Akçalı <oguzkhanakcali@gmail.com>
2023-07-03 04:37:03 +0300

    rev

--- exercise.md
+++ exercise.md
@@ -1,1 +1,5 @@
-Send JSON from Mars
+Change the HTML response
+
+Our HTTP server sends an HTML response body.
+
+Replace the text in the HTML with your own message. Run the server and use your web browser to test your changes.
--- server.mjs
+++ server.mjs
@@ -1,17 +1,15 @@
 import { createServer } from "node:http";
 
 const server = createServer((request, response) => {
-  console.log("request received");
+    console.log("request received");
 
-  response.statusCode = 200;
+    response.statusCode = 200;
 
-  response.setHeader("Content-Type", "application/json");
+    response.setHeader("Content-Type", "text/html");
 
-  const jsonResponseBody = JSON.stringify({ location: "Mars" });
-
-  response.end(jsonResponseBody);
+    response.end("<html><body><h1>This message you see is owned by me</h1></body></html>");
 });
 
 server.listen(3000, () => {
-  console.log(`Server running at http://localhost:3000`);
+    console.log("Server running at http://localhost:3000");
 });
