bd223e5ef507f7df89cb2ec2957e9013f52116c9
Oğuzhan Akçalı <oguzkhanakcali@gmail.com>
2023-07-18 17:32:28 +0300

    CRUD with planets

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,66 @@
 import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;;
 
+app.use(express.json());
 app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-  res.status(200).json({ msg: 'Hello World!' });
+//data
+type Planet = {
+    id: number,
+    name: string,
+};
+
+type Planets = Planet[];
+
+let planets: Planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
+
+
+app.get('/api/planets', (req, res) => {
+  res.status(200).json(planets);
+});
+
+app.get('/api/planets/:id', (req, res) => {
+  const { id } = req.params;
+  const planet = planets.find(p => p.id == Number(id))
+  res.status(200).json(planet);
+});
+
+app.post("/api/planets", (req, res) => {
+  const { id, name } = req.body;
+  const newPlanet = { id, name };
+  planets = [...planets, newPlanet];
+
+  res.status(201).json({ msg: "The planets was created."});
+});
+
+app.put("/api/planets/:id", (req, res) => {
+  const {id} = req.params
+  const {name} = req.body
+  planets = planets.map(p => p.id === Number(id) ? ({...p, name}) : p)
+
+  res.status(200).json({ msg: "The planets has updated." })
+})
+
+app.delete("/api/planets/:id", (req,res) => {
+  const {id} = req.params;
+  planets = planets.filter(p => p.id !== Number(id))
+
+  res.status(200).json({ msg: "The planet was deleted." })
 });
 
 app.listen(port, () => {
